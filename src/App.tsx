7d0fccaef270ad64ead774c60c090318e3ef827e
hungne2k3 <104420895+hungne2k3@users.noreply.github.com>
2024-10-03 20:47:29 +0700

    Call Api axios pokemons

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import { log } from 'console';
 
-type Study = {
+// khai bao type lay ra api name
+type NamePokemons = {
+  name: string;
+  url: string;
+};
+
+type Pokemon = {
   id: number;
   name: string;
-}
+  sprites: {
+    front_default: string;
+  };
+};
 
-type info = Study & {
-  address?: string;
-}
+// định nghĩa type cho hàm thành: React Functional Component React.FC
+const App: React.FC = () => {
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
 
-const people: info = {
-  id: 1,
-  name: 'John',
-}
+  // call Api
+  useEffect(() => {
+    const getPokemons = async () => {
+      const res = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0');
 
-function App() {
+      // lấy toàn bộ tên của các Pokemon
+      res.data.results.forEach(async (pokemon: NamePokemons) => {
+        const poke = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`);
+
+        setPokemons((p) => [...p, poke.data])
+      });
+    }
+
+    getPokemons();
+  }, []);
+
   return (
-    <div className="App">
-
-    </div>
+    (<div className="App">
+      <div className="container">
+        <div className="pokemon-header">
+          <h1>Pokemon ReactJs + TypeScript</h1>
+        </div>
+      </div>
+    </div>)
   );
 }
 
